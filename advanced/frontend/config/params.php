c9ac14982cff8886d69683fe1f6c7b05702c3a9d
Xiaohubb <xiaohubb@hotmail.com>
2014-06-24 06:42:18 -0400

    remove files

--- advanced/frontend/config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'admin@example.com',
-    'loanType' => [\frontend\models\Deal::TYPE_LOAN_UN_AMORTIZED=>'到期本息',
-        \frontend\models\Deal::TYPE_LOAN_AMORTIZED_HBFX=>'付息还本',
-        \frontend\models\Deal::TYPE_LOAN_AMORTIZED_ETP=>'等额本息'],
-];
