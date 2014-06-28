d8df336259d9377c75e23ad32f9315764c1e73a8
Xiaohubb <xiaohubb@hotmail.com>
2014-06-28 10:46:10 -0400

    wechat

--- advanced/frontend/controllers/WechatController.php
+++ advanced/frontend/controllers/WechatController.php
@@ -26,6 +26,19 @@ class WechatController extends Controller
         {
             if ($echostr) exit($echostr);
             file_put_contents(\Yii::$app->runtimePath.'/logs/wechat.log', sprintf("%s\n", trim(file_get_contents('php://input'))), FILE_APPEND);
+            $postStr = trim(file_get_contents('php://input'));
+            if ($postStr)
+            {
+                $message = simplexml_load_string($postStr);
+                $ret = sprintf("<xml>
+<ToUserName>%s</ToUserName>
+<FromUserName>%s</FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType>text</MsgType>
+<Content>%s</Content>
+</xml>", $message->FromUserName, $message->ToUserName, time(),"Hello, Thank you!" );
+                exit($ret);
+            }
         }
     }
 
