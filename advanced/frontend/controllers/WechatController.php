f4022ce595b6446ee6dbdac1292914e6082e39dc
Xiaohubb <xiaohubb@hotmail.com>
2014-06-28 15:33:01 -0400

    wechat

--- advanced/frontend/controllers/WechatController.php
+++ advanced/frontend/controllers/WechatController.php
@@ -20,7 +20,7 @@ class WechatController extends Controller
 
     public function actionIndex($signature = null, $timestamp = null, $nonce = null, $echostr=null)
     {
-        file_put_contents(\Yii::$app->runtimePath.'/logs/wechat.log', sprintf("%s\n", json_encode($_REQUEST)), FILE_APPEND);
+        file_put_contents(\Yii::$app->runtimePath.'/logs/wechat.log', sprintf("%s\n", trim(file_get_contents('php://input'))), FILE_APPEND);
         $this->signature = $signature;
         $this->timestamp = $timestamp;
         $this->nonce = $nonce;
@@ -31,13 +31,7 @@ class WechatController extends Controller
             if ($postStr)
             {
                 $message = simplexml_load_string($postStr);
-                $ret = sprintf("<xml>
-<ToUserName>%s</ToUserName>
-<FromUserName>%s</FromUserName>
-<CreateTime>%s</CreateTime>
-<MsgType>text</MsgType>
-<Content>%s</Content>
-</xml>", $message->FromUserName, $message->ToUserName, time(),"Hello, Thank you!" );
+                $ret = sprintf("<xml><ToUserName><![CDATA[yidaifa123]]></ToUserName><FromUserName><![CDATA[mp_009211212]]></FromUserName><CreateTime>1300921121</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[hellojookee]]></Content><MsgId>1231111121331</MsgId></xml>", $message->FromUserName, $message->ToUserName, time(),"Hello, Thank you!" );
                 exit($ret);
             }
         }
