cf617bfa9be276e311c912542bcee17a265e4c9d
Xiaohubb <xiaohubb@hotmail.com>
2014-07-17 16:05:10 -0400

    add deals view

---
+++ advanced/frontend/views/product/deals.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: al
+ * Date: 7/18/2014
+ * Time: 11:12 AM
+ */
+?>
+<!--main_content start-->
+<div class="main_content" style="">
+    <Div class="financing" style=" background:#f8f8f8">
+        <table class="financingTitle" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <th align="left" width="33%">理财列表</th>
+            </tr>
+            <tr align="center">
+                <td width="33%"><span>12%-16%</span></td>
+            </tr>
+        </table>
+    </Div>
+    <div class="line"></div>
+    <div class="financingList" style="padding-bottom:8px;">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr align="center">
+                <td align="left" colspan="2"><span>协总连锁借贷短期周转</span><img class="icon" src="styles/images/home7_03.png"></td>
+                <td width="17%" rowspan="3"><img src="styles/images/home7_03-02.png"></td>
+            </tr>
+            <tr>
+                <td width="41%">年利率：13%</td>
+                <td width="42%">100万元</td>
+            </tr>
+            <tr>
+                <td colspan="2">期限：6个月</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="financingList" style="padding-bottom:8px;">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr align="center">
+                <td align="left" colspan="2"><span>协总连锁借贷短期周转</span><img class="icon" src="styles/images/home7_03-03.png"></td>
+                <td width="17%" rowspan="3"><div class="zt"><span class="span">已完成</span></div></td>
+            </tr>
+            <tr>
+                <td width="41%">年利率：13%</td>
+                <td width="42%">100万元</td>
+            </tr>
+            <tr>
+                <td colspan="2">期限：6个月</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="financingList" style="padding-bottom:8px;">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr align="center">
+                <td align="left" colspan="2"><span>协总连锁借贷短期周转</span><img class="icon" src="styles/images/home7_03.png"></td>
+                <td width="17%" rowspan="3"><div class="zt zt_1"><span class="span">还款中</span></div></td>
+            </tr>
+            <tr>
+                <td width="41%">年利率：13%</td>
+                <td width="42%">100万元</td>
+            </tr>
+            <tr>
+                <td colspan="2">期限：6个月</td>
+            </tr>
+        </table>
+    </div>
+    <table cellspacing="0" width="100%">
+        <tr>
+            <td align="center"class="" style=" padding:26px;font-size:30px; color:#000;">点击加载更多</td>
+        </tr>
+    </table>
+</div>
+
+
+<!--main_content eng-->
