0f3b849592378749d95f9970beeb9174c047c4e2
Xiaohubb <xiaohubb@hotmail.com>
2014-07-17 15:36:29 -0400

    remove bootstrap dependency

--- advanced/frontend/assets/YdfAsset.php
+++ advanced/frontend/assets/YdfAsset.php
@@ -23,7 +23,7 @@ class YdfAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+//        'yii\bootstrap\BootstrapAsset',
     ];
 
     public $jsOptions = [
